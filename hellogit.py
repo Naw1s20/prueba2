c0a4cf495fbe23c706c7f274a0784d7d0b4d091c
Alex <alejandronrnewey@hotmail.es>
2025-04-15 23:23:01 +0200

    hola probando colorines

--- hellogit.py
+++ hellogit.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-print("probando cosas nuevas V4  he tocado desde la rama login y no debería asique va a dar conflicto")
-=======
-print("probando cosas nuevas V5")
->>>>>>> main
+print("probando cosas nuevas V5asdas")
